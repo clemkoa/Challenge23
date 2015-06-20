f9c0e75676278d3b73d2fca50a0fe519168cbf1e
Clemkoa <Clemkoa>
2015-06-20 11:19:45 +0200

    updating parser shit

--- cjoudet/parser.py
+++ cjoudet/parser.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 import csv
+import re
+import isodate
+
+def parseDuration(word):
+	sum = 0
+	m = re.findall('M([0-9]*)S', word)
+	if m:
+		sum += int(m[0])
+	else:
+		print(word + "	nope	")
+		# m = re.findall('PT([0-9]*)S', word)
+		# return m[0]
+	m = re.findall('([0-9]*)M', word)
+	return sum
+
 
 print('Beginning parsing')
-with open('../challenge_23_data/train_sample.csv', 'rt', encoding='utf-8') as csvfile:
+with open('../../challenge_23_data/train_sample.csv', 'rt', encoding='utf-8') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',')
 	trainingList = list(reader)
 
@@ -26,3 +41,16 @@ for element in trainingList:
 		categories.append(element[0])
 
 print(categories)
+
+i = 0
+tab = [9,12,15]
+for element in trainingList:
+	if i < 100:
+		for j in range(len(element)):
+			if j in tab:
+				if j == 9:
+					print(parseDuration(element[j]), end="")
+				print(element[j] + "	", end="")
+		print()
+	i += 1
+
